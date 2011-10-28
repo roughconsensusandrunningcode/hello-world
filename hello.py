8a8927fe065ed4a1842982b7aa695cdc6abb04e7
Fabrizio Tarizzo <fabrizio@fabriziotarizzo.org>
2011-10-28 19:21:01 +0200

    Added count parameter

--- hello.py
+++ hello.py
@@ -2,5 +2,9 @@
 import sys
 
 if __name__ == '__main__':
-    sys.stdout.write ("Hello World\n")
+    count = 1
+    if len(sys.argv) == 2:
+        count = int(sys.argv[1])
+    for i in xrange (count):
+        sys.stdout.write ("Hello World\n")
 
